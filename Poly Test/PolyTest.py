87993a0028d1992423e9d670cfc8fb5c37cc368b
Ljoseph5454 <ljoseph@ucsb.edu>
2022-12-08 13:12:20 -0800

    3

--- Poly Test/PolyTest.py
+++ Poly Test/PolyTest.py
@@ -4,10 +4,10 @@ import os, pickle
 import sys
 import pandas as pd
 
-df = pd.read_csv('test.csv')
+df = pd.read_csv('EscapeData30.csv')
 print(df.head())
 
-side = 100
+side = -100
 
 hitx = df[df['z']==side]['x'].tolist()
 hity = df[df['z']==side]['y'].tolist()
@@ -15,6 +15,6 @@ hitz = df[df['z']==side]['z'].tolist()
 
 #print(hitx)
 fig, ax = plt.subplots()
-counts, xedges, yedges, im = plt.hist2d(hitx, hity, bins=(2, 2), cmap=plt.cm.jet)
+counts, xedges, yedges, im = plt.hist2d(hitx, hity, bins=(30, 30), cmap=plt.cm.jet)
 fig.colorbar(im, ax=ax)
 plt.show()
