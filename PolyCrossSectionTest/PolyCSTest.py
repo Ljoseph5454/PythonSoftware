bf8d65bcba7aeed27d6992306f707cc058bb106e
Ljoseph5454 <ljoseph@ucsb.edu>
2022-12-16 17:09:33 -0800

    All data

---
+++ PolyCrossSectionTest/PolyCSTest.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os, pickle
+import sys
+import pandas as pd
+
+df = pd.read_csv('Hit8.csv')
+print(df.head())
+
+side = -100
+
+hitx = df[df['Hit']==1]['Hit'].tolist()
+
+print(len(hitx))
